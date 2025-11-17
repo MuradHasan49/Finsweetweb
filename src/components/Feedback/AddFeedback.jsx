3ffce133c8801153b4a570621552ebe584c6bb6d
Murad Hasan <muradhasanshuvo00@gmail.com>
2025-11-18 00:11:05 +0600

    add fix

--- src/components/Feedback/AddFeedback.jsx
+++ src/components/Feedback/AddFeedback.jsx
@@ -1,49 +1,30 @@
 import React from "react";
+import Pic from "../../assets/Feedback/profile.png";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { IoIosArrowForward } from "react-icons/io";
+import { IoIosArrowBack } from "react-icons/io";
 
-function SampleNextArrow(props) {
-  const { className, style, onClick } = props;
+function SampleNextArrow({ onClick }) {
   return (
     <div
-      className={className}
       onClick={onClick}
-      style={{
-        ...style,
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        background: "red",
-        width: "40px",
-        height: "40px",
-        borderRadius: "50%",
-        right: "10px",
-        zIndex: 10,
-      }}
-    />
+      className="absolute top-[200px] right-20 flex justify-center items-center bg-gray-500 w-10 h-10 rounded-full z-10 cursor-pointer hover:bg-gray-700"
+    >
+      <span className="text-white text-xl font-bold"><IoIosArrowForward /></span>
+    </div>
   );
 }
 
-function SamplePrevArrow(props) {
-  const { className, style, onClick } = props;
+function SamplePrevArrow({ onClick }) {
   return (
     <div
-      className={className}
       onClick={onClick}
-      style={{
-        ...style,
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        background: "green",
-        width: "40px",
-        height: "40px",
-        borderRadius: "50%",
-        left: "10px",
-        zIndex: 10,
-      }}
-    />
+      className="absolute top-[200px] left-[610px] flex justify-center items-center bg-gray-500 w-10 h-10 rounded-full z-10 cursor-pointer hover:bg-gray-700"
+    >
+      <span className="text-white text-xl font-bold"><IoIosArrowBack /></span>
+    </div>
   );
 }
 
@@ -58,13 +39,24 @@ function FeedbackSlider() {
   };
 
   return (
-    <div className="slider-container w-96 mx-auto mt-10">
+    <div className="slider-container ">
       <Slider {...settings}>
-     <div className="">
-      <h1>"The best agency we’ve worked with so far. They understand our product and are able to add new features with a great focus."</h1>
-
-      <img src="" alt="" />
-     </div>
+        {[1, 2].map((item) => (
+          <div
+            key={item}
+            className="flex flex-col "
+          >
+            <h1 className="text-gray-700 text-[32px] font-medium leading-12 ">
+              "The best agency we’ve worked with so far. They understand our
+              product and are able to add new features with a great focus."
+            </h1>
+            <img
+              className="mt-10"
+              src={Pic}
+              alt={`Profile ${item}`}
+            />
+          </div>
+        ))}
       </Slider>
     </div>
   );
